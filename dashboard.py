8eca12fb81d13f72c01b885aec4753e278cc6ab8
Maxim Kurkin <maximbider@gmail.com>
2021-12-05 22:51:20 +0300

    add new data & dashboards, fix data generation & layout

--- dashboard.py
+++ dashboard.py
@@ -4,16 +4,25 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+from dash.dependencies import Input, Output
+from pandas.io.parsers import read_csv
+import plotly.express as px
+import numpy as np
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+np.random.seed(0)  # no-display
 app_path = str(pathlib.Path(__file__).parent.resolve())
 df = pd.read_csv(os.path.join(app_path, os.path.join("data", "smarthome.csv")))
+df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv') # for scatter
+df3 = pd.DataFrame({"Col " + str(i+1): np.random.normal(0, np.random.rand(1), size=120) * i for i in range(6)})
 
 app = dash.Dash(__name__, url_base_pathname='/dashboard/')
 server = app.server
 
 theme = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#FFFFFF',
+    'text': '#000000'
 }
 
 
@@ -21,10 +30,11 @@ def build_banner():
     return html.Div(
         className='col-sm-10 row banner',
         children=[
+            html.H5('Maxim Kurkin: Flask assignment', style = {'textAlign': 'center', 'color' : '#FFFFFF'}),
             html.Div(
                 className='banner-text',
                 children=[
-                    html.H5('Enegry Consumption'),
+                    html.H5('ENEGRY CONSUMPTION'),
                 ],
             ),
         ],
@@ -78,6 +88,98 @@ def build_graph():
     )
 
 
+def build_scatter(df):
+    return dcc.Graph(
+        id = 'life-exp-vs-gdp',
+        figure = px.scatter(df, x="population", y="life expectancy",
+                 size="gdp per capita", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+    )
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+def generate_interactive():
+    return html.Div([
+    html.Div(
+        dcc.Graph(id='g1', config={'displayModeBar': False}),
+        className='four columns'
+    ),
+    html.Div(
+        dcc.Graph(id='g2', config={'displayModeBar': False}),
+        className='four columns'
+    ),
+    html.Div(
+        dcc.Graph(id='g3', config={'displayModeBar': False}),
+        className='four columns'
+    ),
+    html.Div(
+        dcc.Graph(id='g4', config={'displayModeBar': False}),
+        className='four columns'
+    )
+    ], className='row')
+# this callback defines 3 figures
+# as a function of the intersection of their 3 selections
+@app.callback(
+    Output('g1', 'figure'),
+    Output('g2', 'figure'),
+    Output('g3', 'figure'),
+    Output('g4', 'figure'),
+    Input('g1', 'selectedData'),
+    Input('g2', 'selectedData'),
+    Input('g3', 'selectedData'),
+    Input('g4', 'selectedData')
+)
+def callback(selection1, selection2, selection3, selection4):
+    selectedpoints = df3.index
+    for selected_data in [selection1, selection2, selection3, selection4]:
+        if selected_data and selected_data['points']:
+            selectedpoints = np.intersect1d(selectedpoints,
+                [p['customdata'] for p in selected_data['points']])
+
+    return [get_figure(df3, "Col 1", "Col 2", selectedpoints, selection1),
+            get_figure(df3, "Col 3", "Col 4", selectedpoints, selection2),
+            get_figure(df3, "Col 5", "Col 6", selectedpoints, selection3),
+            get_figure(df3, "Col 3", "Col 6", selectedpoints, selection4)]
+
+def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
+
+    if selectedpoints_local and selectedpoints_local['range']:
+        ranges = selectedpoints_local['range']
+        selection_bounds = {'x0': ranges['x'][0], 'x1': ranges['x'][1],
+                            'y0': ranges['y'][0], 'y1': ranges['y'][1]}
+    else:
+        selection_bounds = {'x0': np.min(df[x_col]), 'x1': np.max(df[x_col]),
+                            'y0': np.min(df[y_col]), 'y1': np.max(df[y_col])}
+
+    # set which points are selected with the `selectedpoints` property
+    # and style those points with the `selected` and `unselected`
+    # attribute. see
+    # https://medium.com/@plotlygraphs/notes-from-the-latest-plotly-js-release-b035a5b43e21
+    # for an explanation
+    fig = px.scatter(df, x=df[x_col], y=df[y_col], text=df.index)
+
+    fig.update_traces(selectedpoints=selectedpoints,
+                      customdata=df.index,
+                      mode='markers+text', marker={ 'color': 'rgba(0, 116, 217, 0.7)', 'size': 20 }, unselected={'marker': { 'opacity': 0.3 }, 'textfont': { 'color': 'rgba(0, 0, 0, 0)' } })
+
+    fig.update_layout(margin={'l': 20, 'r': 0, 'b': 15, 't': 5}, dragmode='select', hovermode=False)
+
+    fig.add_shape(dict({'type': 'rect',
+                        'line': { 'width': 1, 'dash': 'dot', 'color': 'darkgrey' } },
+                       **selection_bounds))
+    return fig
+
 app.layout = html.Div(
     className='big-app-container',
     children=[
@@ -86,7 +188,13 @@ app.layout = html.Div(
             className='app-container',
             children=[
                 build_graph(),
+                build_scatter(df2),
+                generate_table(df3),
+                generate_interactive()
             ]
         )
     ]
 )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
